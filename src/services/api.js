dd05c3d4c7738ab8055f8f52461989a858570355
RafatAiub <tanvirmahtab1996@gmail.com>
2025-07-24 16:39:02 +0600

    structure: Initial file structure created

---
+++ src/services/api.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+
+const API = axios.create({
+  baseURL: 'https://job-board-project-server-production.up.railway.app/api',
+});
+
+// Optional: attach token
+API.interceptors.request.use((config) => {
+  const token = localStorage.getItem('token');
+  if (token) config.headers.Authorization = `Bearer ${token}`;
+  return config;
+});
+
+export default API;
